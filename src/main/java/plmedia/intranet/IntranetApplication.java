10d7c1347bc9dc9651e330c5cc515588e10589c1
Jonas Ladefoged Holm <jonas@holm.tech>
2017-10-25 11:18:31 +0200

    init

---
+++ src/main/java/plmedia/intranet/IntranetApplication.java
@@ -0,0 +1,12 @@
+package plmedia.intranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetApplication.class, args);
+	}
+}
