cb4adc6437e10cd8b9c72dbf93a6e7c36c36162b
Jonas Ladefoged Holm <jonas@holm.tech>
2017-11-14 12:42:21 +0100

    JDBC Authentication og Password Hashing
    Indtil videre er det kun en standard user der er gemt i databasen, men brug mysql dumpen og tjek application.properties

--- src/main/java/plmedia/intranet/IntranetApplication.java
+++ src/main/java/plmedia/intranet/IntranetApplication.java
@@ -1,12 +1,42 @@
 package plmedia.intranet;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IntranetApplication {
 
+	@Value("${spring.datasource.driverClassName}")
+	private String databaseDriverClassName;
+
+	@Value("${spring.datasource.url}")
+	private String datasourceUrl;
+
+	@Value("${spring.datasource.username}")
+	private String databaseUsername;
+
+	@Value("${spring.datasource.password}")
+	private String databasePassword;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(IntranetApplication.class, args);
+
+
+
+	}
+
+	@Bean
+	public DataSource datasource() {
+		org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
+		ds.setDriverClassName(databaseDriverClassName);
+		ds.setUrl(datasourceUrl);
+		ds.setUsername(databaseUsername);
+		ds.setPassword(databasePassword);
+
+		return ds;
 	}
 }
--- src/main/java/plmedia/intranet/security/WebSecurityConfig.java
+++ src/main/java/plmedia/intranet/security/WebSecurityConfig.java
@@ -1,16 +1,29 @@
 package plmedia.intranet.security;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import java.net.PasswordAuthentication;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource datasource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,11 +38,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder myAuth) throws Exception {
+
+        /**
+         * Tak til https://justinrodenbostel.com/2014/05/30/part-5-integrating-spring-security-with-spring-boot-web/
+         * for den gode tutorial på at gøre jdbc authentication mulig.
+         */
+
+        //laver instances af userdetailsservice og passwordencoder
+        JdbcUserDetailsManager myUserDetailsService = new JdbcUserDetailsManager();
+        PasswordEncoder myEncoder = new BCryptPasswordEncoder();
+
+        /*indstiller datasourcen til vores database via datasource (kig i vores MAIN "IntranetApplication"
+          det er der den bliver lavet ud fra vores indstillinger i application.properties)
+        */
+        myUserDetailsService.setDataSource(datasource);
+
+        //hiver fat i vores authentication manager, og tilknytter vores userdetails service og password encoder
+        myAuth.userDetailsService(myUserDetailsService).passwordEncoder(myEncoder);
+
+        //vi gør brug af jdbc authentication
+        myAuth.jdbcAuthentication().dataSource(datasource);
+
+        if(!myUserDetailsService.userExists("admin")) {
+            List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+            User myUserDetails = new User("autouser", myEncoder.encode("123"),authorities);
+            myUserDetailsService.createUser(myUserDetails);
+        }
 
 
-
-        auth
+        myAuth
                 //.userDetailsService(userDetailsService).and()
                 .inMemoryAuthentication()
                     .withUser("user").password("123").roles("USER")
