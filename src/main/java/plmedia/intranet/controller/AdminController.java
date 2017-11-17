01bb08f03d08b719c5ffb6c4e112a8c875f24313
Jonas Ladefoged Holm <jonas@holm.tech>
2017-11-17 14:15:39 +0100

    Små rettelser bla javadoc

--- src/main/java/plmedia/intranet/controller/AdminController.java
+++ src/main/java/plmedia/intranet/controller/AdminController.java
@@ -1,19 +1,14 @@
 package plmedia.intranet.controller;
-
-
 import java.security.Principal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import plmedia.intranet.model.Employee;
-import plmedia.intranet.model.User;
 
-import java.util.ArrayList;
+/**
+ * @author Jonas Holm
+ * */
 
 @Controller
 public class AdminController {
