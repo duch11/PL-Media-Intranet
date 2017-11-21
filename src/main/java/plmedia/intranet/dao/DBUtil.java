9bee9ecc13d0e67127017105115bf2a9ba056710
tobi5519 <tobi5519@stud.kea.dk>
2017-11-21 21:50:27 +0100

    Added DBUtil class

--- src/main/java/plmedia/intranet/dao/DBUtil.java
+++ src/main/java/plmedia/intranet/dao/DBUtil.java
@@ -4,6 +4,8 @@ import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -17,13 +19,23 @@ public class DBUtil extends JdbcUserDetailsManager {
 
     // SQL's
     public static final String DEF_CREATE_ALLERGEN_SQL = "insert into allergen (allergen_name, allergen_description) values (?,?)";
+    public static final String DEF_ADD_ALLERGEN_SQL = "insert into child_allergen (fk_child_id, fk_allergen_id) values (?,?)";
 
+    public static final String CALL_TEST = "{CALL test_sp(?)}";
 
     // Fields
     private String createAllergenSql = DEF_CREATE_ALLERGEN_SQL;
+    private String addAllergenSql = DEF_ADD_ALLERGEN_SQL;
 
+    private String test = CALL_TEST;
 
     // Methods
+
+    /**
+     * Creates a new allergen, adding it to the database
+     * @param name
+     * @param description
+     */
     public void createAllergen(String name, String description) {
 
         getJdbcTemplate().update(createAllergenSql, new PreparedStatementSetter() {
@@ -35,5 +47,34 @@ public class DBUtil extends JdbcUserDetailsManager {
         });
     }
 
+    /**
+     * Adds an existing allergen to a child
+     * @param child_id
+     * @param allergen_id
+     */
+    public void addAllergen(int child_id, int allergen_id){
+        getJdbcTemplate().update(addAllergenSql, new PreparedStatementSetter() {
+            // Anonymous method
+            public void setValues(PreparedStatement ps) throws SQLException {
+                ps.setInt(1, child_id);
+                ps.setInt(2, allergen_id);
+            }
+        });
+    }
 
+    /**
+     * Why Doesn't Glue Stick To The Inside Of The Glue Bottle?
+     * @param id
+     */
+    public void test(String id){
+        try(
+            Connection conn = ConnectionManager.getConnection();
+            CallableStatement statement = conn.prepareCall(test);
+        ) {
+            statement.execute();
+            statement.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
