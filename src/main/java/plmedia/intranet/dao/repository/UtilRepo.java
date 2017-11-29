57c1101024abead087f74c181d5b9f8d8dc4204d
tobi5519 <tobi5519@stud.kea.dk>
2017-11-29 11:40:34 +0100

    added checkemail to UtilRepo and refractored ConMon

--- src/main/java/plmedia/intranet/dao/repository/UtilRepo.java
+++ src/main/java/plmedia/intranet/dao/repository/UtilRepo.java
@@ -1,8 +1,12 @@
 package plmedia.intranet.dao.repository;
 
+import ch.qos.logback.core.db.dialect.DBUtil;
+import plmedia.intranet.dao.DBUtil.Util;
 import plmedia.intranet.model.Wing;
 
 public class UtilRepo {
+
+  Util util = new Util();
 
   public int createWing() {
     return 0;
@@ -10,6 +14,10 @@ public class UtilRepo {
 
   public Wing readWing(int i) {
     return null;
+  }
+
+  public int checkEmail(String email){
+    return util.checkEmail(email);
   }
 
   
