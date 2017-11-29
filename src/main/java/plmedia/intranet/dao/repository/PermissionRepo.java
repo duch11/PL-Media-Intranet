48f15bce7afb76389a90924a5caaef6e6c9b09d4
tobi5519 <tobi5519@stud.kea.dk>
2017-11-29 22:42:07 +0100

    Added tom PermissionRepo

---
+++ src/main/java/plmedia/intranet/dao/repository/PermissionRepo.java
@@ -0,0 +1,23 @@
+package plmedia.intranet.dao.repository;
+
+import java.util.ArrayList;
+import plmedia.intranet.model.Permission;
+
+public class PermissionRepo {
+
+  public Permission readPermissionByID(int id){
+
+    return null;
+  }
+
+  public ArrayList<Permission> readAllPermissions(){
+
+    return null;
+  }
+
+  public ArrayList<Permission> readPermissionsByUserID(int i){
+
+    return null;
+  }
+
+}
