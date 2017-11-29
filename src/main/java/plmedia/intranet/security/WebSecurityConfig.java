252a9cf86f6f5eb802ed2f55b07b6573b8fd9b32
Jonas Ladefoged Holm <jonas@holm.tech>
2017-11-29 13:08:11 +0100

    Create parent form virker nu. Dog ikke dynamisk genereret. Pænere..

--- src/main/java/plmedia/intranet/security/WebSecurityConfig.java
+++ src/main/java/plmedia/intranet/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author Jonas holm
@@ -59,6 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
          * Ellers virker det, og vi tjekker op mod user_email.
          * */
         auth.jdbcAuthentication().dataSource(dataSource)
+                /*.passwordEncoder(new BCryptPasswordEncoder())*/
             .usersByUsernameQuery(
                 "select user_email,password, enabled from user where user_email=?")
             .authoritiesByUsernameQuery(
