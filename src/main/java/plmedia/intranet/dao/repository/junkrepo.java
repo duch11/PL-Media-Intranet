523efc2c33bbe9791174d706d75fdbb8fe202601
tobi5519 <tobi5519@stud.kea.dk>
2017-12-14 22:45:35 +0100

    Update junkrepo.java

--- src/main/java/plmedia/intranet/dao/repository/junkrepo.java
+++ src/main/java/plmedia/intranet/dao/repository/junkrepo.java
@@ -6,7 +6,7 @@ public class ChildRepo implements IRepo<Child> {
 
   public int Create(Child child){ ... }
     
-  public T Read(int id){ ... }
+  public Child Read(int id){ ... }
 
   public int Update(Child child){ ... }
 
