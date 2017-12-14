53b78764330d791e05bcd5f04add1fee6e049b71
tobi5519 <tobi5519@stud.kea.dk>
2017-12-14 22:22:03 +0100

    Update Junk_CodeSnippets.java

--- src/main/java/plmedia/intranet/dao/DBUtil/Junk_CodeSnippets.java
+++ src/main/java/plmedia/intranet/dao/DBUtil/Junk_CodeSnippets.java
@@ -1,23 +1,18 @@
  try (
         Connection con = ConMan.getConnection()
     ) {
+      PreparedStatement stmt = ConMan.prepStat(con, Statements.DEF_FIND_USER_BY_NAME);
+      stmt.setString(1, name);
+  
+      ResultSet rs = stmt.executeQuery();
 
-      // code
+      System.out.println(
+        "ID: " + rs.getInt("id) +
+        "Name: " + rs.getString("name") +
+        "Birthday: " + rs.getString("name") + 
+        "Nickname: " + rs.getString("nickname"));
    
     } catch (SQLException e) {
       processException(e);
     }
   }
-
-
-      PreparedStatement stmt = ConMan.prepStat(con, Statements.DEF_FIND_USER_BY_NAME);
-      stmt.setString(1, name);
-
-
-      ResultSet rs = stmt.executeQuery();
-
-System.out.println(
- "ID: " + rs.getInt("id) +
- "Name: " + rs.getString("name") +
- "Birthday: " + rs.getString("name") + 
- "Nickname: " + rs.getString("nickname"));
