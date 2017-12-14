2eea41bf219e3b01f0daf6639d6220e9673171d9
tobi5519 <tobi5519@stud.kea.dk>
2017-12-14 21:41:30 +0100

    Update Junk_CodeSnippets.java

--- src/main/java/plmedia/intranet/dao/DBUtil/Junk_CodeSnippets.java
+++ src/main/java/plmedia/intranet/dao/DBUtil/Junk_CodeSnippets.java
@@ -8,3 +8,13 @@
       processException(e);
     }
   }
+
+
+      PreparedStatement stmt = ConMan.prepStat(con, Statements.SQL);
+
+
+      stmt.setString(1, "name");
+
+
+      ResultSet rs = stmt.executeQuery();
+
